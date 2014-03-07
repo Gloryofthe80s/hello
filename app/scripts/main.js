49cd379373402521c8d4ef41ed664e87c477e059
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-03-07 00:59:00 -0500

    initialized with significant progress

---
+++ app/scripts/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function($) {
+
+    console.log("I feel pretty.");
+
+    $('.tooltip').tooltipster({
+        theme: 'tooltipster-shadow'
+    });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
