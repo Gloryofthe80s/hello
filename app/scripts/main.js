52b40cb6cc4d7c5d629734293edda5938777e8d7
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-03-08 15:54:09 -0500

    Edited copy; tooltipster is working and styled

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,9 @@
 $(document).ready(function($) {
 
-    console.log("I feel pretty.");
-
-    $('.tooltip').tooltipster({
-        theme: 'tooltipster-shadow'
+    $('.tip').tooltipster({
+        theme: 'tooltipster-shadow',
+        delay: 0
     });
-
 
 
 });
